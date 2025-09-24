b8a6d1fbfe3b14a0c07a374f92e27f54b56625ac
dstark12 <143757465+dstark12@users.noreply.github.com>
2025-09-23 21:26:12 -0400

    Home page basic, file restructuring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,47 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { listings } from "@/data/listings";
+import ListingCard from "@/components/cards";
 
 export default function Home() {
   return (
     <div>
-      <div className="topNav">
+      <div className="topNav flex justify-between items-center bg-gray-800">
         <div className="text-lg font-semibold text-white p-4">CommonGround</div>
-        <div>
-          <a className="active" href="/">Home</a>
-          <a href="/contact">Contact</a>
-          <a href="/about">About</a>
+        <div className="space-x-4 text-white p-4">
+          <Link href="/contact">Contact</Link>
+          <Link href="/about">About</Link>
         </div>
       </div>
-      <div className="flex justify-center p-4 h-screen">
-        <form>
-            <label>I'm looking for:</label>
-            <input className="border" type="text"></input>
+          
+      <div className="relative w-full h-[500px]">
+        <Image
+          src="/home.jpg"
+          alt="Marketplace"
+          fill
+          className="object-cover"
+        />
+        
+        {/* Overlay content */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/60 ">
+          <h1 className="text-white text-3xl font-bold">Welcome to CommonGround</h1>
+          <form>
+            <label className="text-white">I'm looking for: </label>
+            <input className=" text-black rounded-full bg-white w-60" type="text" placeholder="University room"></input>
         </form>
         </div>
+      </div>
+        
+      <div className="bg-black">
+      <h1 className="text-white text-3xl font-mono font-extrabold ml-8 p-4">Listings near me 📍</h1>
+
+      <div className="flex flex-wrap gap-6 justify-center p-8">
+        {listings.map((listing) => (
+        <ListingCard key={listing.id} {...listing} />
+        ))}
+       </div>
+      </div>
     </div>
   );
 }
